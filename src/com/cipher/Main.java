3250abcf38af48bb1fe65620885511138e99636b
Maksym Korniienko <maksym.korniienko25@gmail.com>
2024-01-26 03:49:26 +0200

    Task_1; managing program flow

--- src/com/cipher/Main.java
+++ src/com/cipher/Main.java
@@ -1,40 +1,10 @@
 package com.cipher;
 
-import com.cipher.model.Mode;
-import com.cipher.service.BruteForceService;
-import com.cipher.service.DecryptService;
-import com.cipher.service.EncryptService;
-import com.cipher.validation.InputValidation;
+import com.cipher.controller.ProgramController;
 
 public class Main {
-    private static final int COMMAND = 0;
-    private static final int FILE_PATH = 1;
-    private static final int KEY = 2;
-    private static final int ANALYSIS_FILE_PATH = 2;
 
     public static void main(String[] args) {
-        InputValidation.argsNumber(args.length);
-        String command = args[COMMAND];
-        String path = args[FILE_PATH];
-        Mode mode = InputValidation.command(command);
-        InputValidation.filePath(path);
-        switch (mode) {
-            case ENCRYPTION -> {
-                int key = InputValidation.key(args[KEY]);
-                EncryptService encryption = new EncryptService(path);
-                encryption.encryptFile(key);
-            }
-            case DECRYPTION -> {
-                int key = InputValidation.key(args[KEY]);
-                DecryptService decryption = new DecryptService(path);
-                decryption.decryptFile(key);
-            }
-            case BRUTE_FORCE -> {
-                String analysisPath = args[ANALYSIS_FILE_PATH];
-                InputValidation.filePath(analysisPath);
-                BruteForceService bruteForcing = new BruteForceService(path, analysisPath);
-                bruteForcing.bruteForceFile();
-            }
-        }
+        new ProgramController(args).run();
     }
 }
---
+++ src/com/cipher/controller/ProgramController.java
@@ -0,0 +1,52 @@
+package com.cipher.controller;
+
+import com.cipher.model.Mode;
+import com.cipher.service.BruteForceService;
+import com.cipher.service.CipherService;
+import com.cipher.service.DecryptService;
+import com.cipher.service.EncryptService;
+import com.cipher.validation.InputValidation;
+
+public class ProgramController {
+    private static final int COMMAND_POSITION = 0;
+    private static final int FILE_PATH_POSITION = 1;
+    private static final int KEY_POSITION = 2;
+    private static final int ANALYSIS_FILE_PATH_POSITION = 2;
+
+    private String[] args;
+
+    public ProgramController(String[] args) {
+        this.args = args;
+    }
+
+    private CipherService getCipherInstance(Mode mode, String path) {
+        switch (mode) {
+            case ENCRYPTION -> {
+                int key = InputValidation.key(args[KEY_POSITION]);
+                return new EncryptService(path, key);
+            }
+            case DECRYPTION -> {
+                int key = InputValidation.key(args[KEY_POSITION]);
+                return new DecryptService(path, key);
+            }
+            case BRUTE_FORCE -> {
+                String analysisPath = args[ANALYSIS_FILE_PATH_POSITION];
+                InputValidation.filePath(analysisPath);
+                return new BruteForceService(path, analysisPath);
+            }
+            default -> {
+                return null;
+            }
+        }
+    }
+
+    public void run() {
+        InputValidation.argsNumber(args.length);
+        String command = args[COMMAND_POSITION];
+        String path = args[FILE_PATH_POSITION];
+        Mode mode = InputValidation.command(command);
+        InputValidation.filePath(path);
+        CipherService service = getCipherInstance(mode, path);
+        service.cipher();
+    }
+}
